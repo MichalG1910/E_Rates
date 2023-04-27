0c9887427be67e54756924972ea854e3ab260466
bioly1910 <grabarzmichal@gmail.com>
2023-04-27 11:29:14 +0200

    dodano toolTips

---
+++ classE_Rates101_Tooltip.py
@@ -0,0 +1,36 @@
+#======================
+# imports
+#======================
+import tkinter as ttk
+
+class ToolTip(object): 
+    def __init__(self, widget):
+        self.widget = widget
+        self.tipwindow = None
+        self.id = None
+        self.x = self.y = 0
+
+    def showtip(self, text, changeCoordinatesX, changeCoordinatesY): 
+        "Display text in tooltip window"
+        self.text = text 
+        if self.tipwindow or not self.text:
+            return
+        x, y, _cx, cy = self.widget.bbox("insert") 
+        x = x + self.widget.winfo_rootx() + 27 + changeCoordinatesX
+        y = y + cy + self.widget.winfo_rooty() +27 + changeCoordinatesY
+        self.tipwindow = tw = ttk.Toplevel(self.widget) 
+        tw.wm_overrideredirect(1)
+        tw.wm_geometry(f"+{x}+{y}") 
+
+        label = ttk.Label(tw, text=self.text, justify=ttk.LEFT,
+                      background="grey", foreground="black", relief=ttk.SOLID, borderwidth=1,
+                      font=("Segoe Ui", "10", "normal"))
+        label.grid(ipadx=1) 
+
+    def hidetip(self): 
+        tw = self.tipwindow
+        self.tipwindow = None
+        if tw:
+            tw.destroy()
+            
+
